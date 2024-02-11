67abc0bcc48d20a4232630c1c2dde75c10afc4f8
SahilPatel650 <sahil.va.us@gmail.com>
2024-02-11 04:28:43 -0500

    added status

--- react-flask-app/src/Components/Server/Callitem.js
+++ react-flask-app/src/Components/Server/Callitem.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import './CallItem.css'; 
 
 const CallItem = ({ call, data, isActive, onClick }) => {
+
+    const getTriageColor = (triage) => {
+        switch(triage) {
+            case 1: return 'red';
+            case 2: return 'orange';
+            case 3: return 'yellow';
+            case 4: return 'lightgreen';
+            case 5: return 'green';
+            default: return 'grey';
+        }
+    };
+
     return (
+        
         <div className="call-item">
+            <div className="triage-dot" style={{ backgroundColor: getTriageColor(data.triage) }}></div>
             <p onClick={onClick}><strong>Call ID:</strong> {call.callid}</p>
             {isActive && (
                 <div className="call-details">
                     {/* Add more details here */}
                     <p>{"Transcript: " + data.transcript}</p>
                     <p>{"Age: " + (data.age || "Unknown") }</p>
+                    <p>{"Is Inpatient: " + (data.admit) }</p>
                     <p>{"Gender: " + (data.gender || "Unknown") }</p>
                     <p>{"Complaints: " + (data.complaints || []).join(", ") }</p>
                     <p>{"History: " + (data.history || []).join(", ") }</p>
